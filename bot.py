fcd9af25a21e12dea6b10f3996c1f5c9b6cde927
B <git@pasuta.com>
2021-01-11 23:30:56 +0900

    プロフィール画像の変更チェック

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ ex_follow = []
 ex_rt = []
 
 init_json = {"ids": dic, "followings": dic,
-             "favorites": dic, "statuses_count": dic}
+             "favorites": dic, "statuses_count": dic, "profile_image_url": dic}
 
 
 # post_channel_config = ['考察1st', 'bot用','｢reaper｣-｢unknown｣-｢i｣監視']
@@ -152,6 +152,7 @@ def get_latest_tweets(screen_name, idx, count):
 
     message = []
     id = 0
+    profimg = ''
     provisional_str = PROV_STR if idx >= provisional_line and my_model_no == 2 else ''
 
     # 死神だけは何も返ってこない
@@ -166,11 +167,15 @@ def get_latest_tweets(screen_name, idx, count):
             n = statuses_count - latest_dic["statuses_count"][str(idx)]
             latest_dic["statuses_count"][str(idx)] = statuses_count
             if n > 1:
-                message, id = get_latest_tweets(screen_name, idx, n)
+                message, id, profimg = get_latest_tweets(screen_name, idx, n)
                 message.reverse()
                 return message, id
 
         for line in timelines:  # タイムラインリストをループ処理
+
+            if not line['retweeted']:
+                img = line['user']['profile_image_url_https']
+                profimg = img.replace('_normal.', '.')
 
             message.append('{2}https://twitter.com/{0}/status/{1}'.format(
                 screen_name, line['id_str'], provisional_str))
@@ -182,7 +187,7 @@ def get_latest_tweets(screen_name, idx, count):
         message = "Failed: %d" % res.status_code
         id = -1
 
-    return message, id
+    return message, id, profimg
 
 
 def get_limit():
@@ -269,7 +274,7 @@ class MiyaClient(discord.Client):
     def tweet_report(self):
 
         for k, v in dic.items():
-            urls, id = get_latest_tweets(v, k, 1)
+            urls, id, profimg = get_latest_tweets(v, k, 1)
             following, name, fav = get_followings(v)
 
             if id < 0 or following < 0:
@@ -291,6 +296,15 @@ class MiyaClient(discord.Client):
                             self.q.put(i)
 
                     latest_dic["ids"][str(k)] = id
+
+                if MODEL_NO_2_ENABLE and profimg != latest_dic["profile_image_url"][str(k)]:
+                    latest_dic["flags"]["update_count"] += 1
+
+                    ss = '{0} のプロフィール画像が変更されました\n{1}'.format(v, profimg)
+                    self.q.put(ss)
+                    print(ss)
+
+                    latest_dic["profile_image_url"][str(k)] = profimg
 
                 if following != latest_dic["followings"][str(k)]:
                     latest_dic["flags"]["update_count"] += 1
