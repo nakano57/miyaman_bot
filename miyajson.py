4a27e5c9356c16fe5bd8c0e35eb78be97169589e
anastasia <hiroto856@gmail.com>
2021-01-13 01:19:37 +0900

    split

---
+++ miyajson.py
@@ -0,0 +1,31 @@
+import sys
+import json
+
+
+class Miyajson():
+    dic = {0: "binarycity_i",
+           1: "strawberry_fore",
+           2: "actress_nanano",
+           3: "kirakira_nanano",
+           4: "riya_hoshino",
+           5: "aoshima_rokusen",
+           6: "iwanaga_sizu",
+           7: "hikari_miyaman",
+           8: "reaper_surveill",
+           9: "Ft3oh35Hy51d",
+           10: "waruichigo",
+           11: "reiko_blueislan",
+           12: "retanihoshino",
+           13: "castleseven_aya"
+           }
+
+    init_json = {"ids": dic, "followings": dic,
+                 "favorites": dic, "statuses_count": dic,
+                 "profile_image_url": dic, "profile_banner_url": dic}
+
+    def __init__(self) -> None:
+        with open(sys.argv[1]) as f:
+            try:
+                self.latest_dic = json.load(f)
+            except:
+                self.latest_dic = Miyajson.init_json
